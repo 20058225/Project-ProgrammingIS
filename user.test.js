8e06a97646f9a85dca658ece60ee7430377c8aef
Brenda Lopes <20058225@mydbs.ie>
2024-12-09 08:42:53 +0000

    Update user.test.js

--- user.test.js
+++ user.test.js
@@ -26,27 +26,30 @@ describe('User CRUD API', function () {
     });
 
     it('should add a user successfully', async function () {
-        const res = await chai
-        .request(app)
-        .post('/addUser')  
-        .send({ username: user.userFullName, email: user.userEmail, password: user.userPassword })
-        .expect(200);
+        connectionMock.resolves([{ affectedRows: 1 }]); // Mock DB response
 
+        const res = await chai
+            .request(app)
+            .post('/addUser')  
+            .send({ username: user.userFullName, email: user.userEmail, password: user.userPassword })
+            
         expect(res).to.have.status(200);
         expect(res.body).to.have.property('message', 'User added successfully.');
     });
     
     it('should fail to add a user if fields are missing', async function () {
         const res = await chai
-        .request(app)
-        .post('/addUser')
-        .send({ username: user.userFullName, email: user.userEmail })
+            .request(app)
+            .post('/addUser')
+            .send({ username: user.userFullName, email: user.userEmail })
 
         expect(res).to.have.status(400);
         expect(res.text).to.equal('Username, email, and password are required.');
     });
 
     it('should delete a user successfully', async function () {
+        connectionMock.resolves([{ affectedRows: 1 }]); // Mock DB response
+
         const res = await chai
             .request(app)
             .delete('/deleteUser/1')  // Ensure route matches
